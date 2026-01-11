bf30b995a6ea08ab7793f2757ec01a4819876547
miguel-vila <miguel@invariante.co>
2026-01-11 15:13:46 -0500

    rm unused

--- apps/web/components/rates-table.tsx
+++ apps/web/components/rates-table.tsx
@@ -11,14 +11,7 @@ import {
   type SortingState,
   type ColumnFiltersState,
 } from "@tanstack/react-table";
-import {
-  type Offer,
-  BankNames,
-  type BankId,
-  ProductType,
-  CurrencyIndex,
-  Segment,
-} from "@mejor-tasa/core";
+import { type Offer, ProductType, CurrencyIndex, Segment } from "@mejor-tasa/core";
 import { formatRate, formatDateTime } from "@/lib/format";
 
 export function RatesTable() {
